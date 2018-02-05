a275a212d2f8d8f7d681e0e802cd2a0496864bb3
S-heep <991207691@qq.com>
2018-02-05 17:10:28 +0800

    【LogManager】日志显示

--- LogManager/DetailLoggerViewController.swift
+++ LogManager/DetailLoggerViewController.swift
@@ -10,26 +10,25 @@ import UIKit
 
 class DetailLoggerViewController: UIViewController {
 
+    var logContent: String!
+    var textView: UITextView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        initTextView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func initLogContent(logString: String) {
+        logContent = logString
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func initTextView() {
+        textView = UITextView.init(frame: view.frame)
+        textView.autoresizingMask = UIViewAutoresizing(rawValue: UIViewAutoresizing.RawValue(UInt8(UIViewAutoresizing.flexibleHeight.rawValue) | UInt8(UIViewAutoresizing.flexibleWidth.rawValue)))
+        textView.isEditable = false
+        textView.text = logContent
+        view.addSubview(textView)
     }
-    */
+
 
 }
